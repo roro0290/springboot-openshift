3743b31dc5d026158e14628f3a5e9c6fbd5719d1
Roro0290 <rohini_subramanian@outlook.com>
2022-12-03 14:04:42 +0700

    ColorController recommends a random color to wear

---
+++ src/main/java/com/example/springboot/openshift/sample1/ColorController.java
@@ -0,0 +1,32 @@
+package com.example.springboot.openshift.sample1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.random.RandomGenerator;
+
+
+@RestController
+public class ColorController {
+
+    @GetMapping("/color")
+    public String chooseColor(){
+        String color = "";
+        RandomGenerator rgen = RandomGenerator.getDefault();
+        switch(rgen.nextInt(0,3)){
+            case 1:
+                color = "red";
+                break;
+            case 2:
+                color = "blue";
+                break;
+            case 3:
+                color = "green";
+                break;
+            default:
+                color = "none";
+        }
+        return color;
+    }
+
+}
